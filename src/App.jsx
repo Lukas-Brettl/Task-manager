9a4df6c804023a9570e9660379edb4f887499af0
Lukas <lukas.brettl@seznam.cz>
2024-05-08 09:20:30 +0200

    Add structure of App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <header className="flex justify-center">
+        <div>App</div>
+
+      </header>
+      <section>
+        <main></main>
+      </section>
+      
+      <footer>
+
+      </footer>
+    </>
+  );
+};
+
+export default App;
