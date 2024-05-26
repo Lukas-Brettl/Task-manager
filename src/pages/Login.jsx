fc9e36bf60d2e374218589b78528ecdf88ea5611
brettl <lukas.brettl@seznam.cz>
2024-05-26 19:55:44 +0200

    Add register page with box-shadow

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,10 @@ const Login = ()=>{
     const [showpassword, setShowpassword] = useState(false)
     const [name, setName] = useState()
     const [password, setPassword] = useState()
+    const [focusn, setFocusn] = useState(false)
+    const [focusp, setFocusp] = useState(false)
+    const [focuse, setFocuse] = useState(false)
+    const [sign, setSign] = useState(true)
 
     function welcome(){
         if(name && password){
@@ -24,24 +28,53 @@ const Login = ()=>{
 
     return(
         <div id="main-login-div" className="flex justify-center items-center">
+            {sign ? 
             <main className="text-white rounded-lg flex flex-col items-center">
                 
                     <span className="text-2xl mb-24 mt-8 font-medium">
-                        <button>Login</button>
+                        <button onClick={() =>setSign(true)}>Login</button>
                         /
-                        <button>Register</button>
+                        <button onClick={()=>setSign(false)}>Register</button>
                     </span>
                     <div>
                     <label className="font-semibold mt-14">User name:</label><br/>
-                    <input name="uname" onChange={(e) =>(setName(e.target.value))} className="rounded-md mb-3 h-8 outline-none bg-slate-600 p-2"/><br/>
+                    <input name="uname" onFocus={() =>setFocusn((focus) => !focus)} onBlur={() => setFocusn(false)}
+                        style={focusn ? {boxShadow: "0 0 3px #558dff"} : null} onChange={(e) =>(setName(e.target.value))} className="rounded-md mb-3 h-8 w-56 outline-none p-2" required/><br/>
                     <label  className="font-semibold ">User password:</label><br/>
                     <span className="flex items-center">
-                        <input name="upassword" onChange={(e) =>(setPassword(e.target.value))} type={showpassword ? "text" : "password"} className="rounded-md h-8 outline-none bg-slate-600 p-2"/>
-                        <input className="w-5 h-5 ml-8 outline-none bg-slate-600" type="checkbox" onClick={() => setShowpassword((show) => !show)}/>
+                        <input name="upassword" onFocus={() =>setFocusp((focus) => !focus)} onBlur={() => setFocusp(false)}
+                        style={focusp ? {boxShadow: "0 0 3px #558dff"} : null} onChange={(e) =>(setPassword(e.target.value))} className="rounded-md mb-3 h-8 w-56 outline-none p-2" type={showpassword ? "text" : "password"} required/>
+                        <input className="w-5 h-5 ml-8 outline-none " type="checkbox" onClick={() => setShowpassword((show) => !show)}/>
                     </span>
                     <button className="p-5 rounded-md text-lg font-semibold flex justify-center items-center mt-8 " id="button-account" onClick={welcome}>Create account</button>
                 </div>
             </main>
+            
+            :
+            
+            <main className="text-white rounded-lg flex flex-col items-center">
+                
+                    <span className="text-2xl mb-14 mt-8 font-medium">
+                        <button onClick={() =>setSign(true)}>Login</button>
+                        /
+                        <button onClick={()=>setSign(false)}>Register</button>
+                    </span>
+                <div>
+                    <label className="font-semibold mt-14">User name:</label><br/>
+                    <input name="uname" onFocus={() =>setFocusn((focus) => !focus)} onBlur={() => setFocusn(false)}
+                        style={focusn ? {boxShadow: "0 0 3px #558dff"} : null} onChange={(e) =>(setName(e.target.value))} className="rounded-md mb-3 h-8 w-56 outline-none p-2" required/><br/>
+                    <label className="font-semibold mt-14">E-mail:</label><br/>
+                    <input type="email" name="email" onFocus={() =>setFocuse((focus) => !focus)} onBlur={() => setFocuse(false)}
+                        style={focuse ? {boxShadow: "0 0 3px #558dff"} : null} className="rounded-md mb-3 h-8 w-56 outline-none p-2" required/><br/>
+                    <label  className="font-semibold ">Password:</label><br/>
+                    <span className="flex items-center">
+                        <input name="upassword" onFocus={() =>setFocusp((focus) => !focus)} onBlur={() => setFocusp(false)}
+                        style={focusp ? {boxShadow: "0 0 3px #558dff"} : null} onChange={(e) =>(setPassword(e.target.value))} className="rounded-md mb-3 h-8 w-56 outline-none p-2" type={showpassword ? "text" : "password"} required/>
+                        <input className="w-5 h-5 ml-8 outline-none " type="checkbox" onClick={() => setShowpassword((show) => !show)}/>
+                    </span>
+                    <button className="p-5 rounded-md text-lg font-semibold flex justify-center items-center mt-8 " id="button-account" onClick={welcome}>Create account</button>
+                </div>
+            </main>}
             <Toaster />
         </div>
     )
